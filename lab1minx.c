71da49f2ef3f1d6429e2fc97df8e57a5b8e33a8c
6704062610089 <s6704062610089@email.kmutnb.ac.th>
2024-11-26 14:54:34 +0700

    ข้อ 1 ครับ

---
+++ lab1minx.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int number1 = 50;
+	int number2 = 10;
+	if (number1 > number2) {
+	printf("number1 is greater than number2");
+	}else{
+	printf("number2 is greater than number1");
+	}
+}
