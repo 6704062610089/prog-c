867a8881a0690c032931db82081da7c183f3bbbd
6704062610089 <s6704062610089@email.kmutnb.ac.th>
2024-11-26 21:17:40 +0700

    ข้อ 3 แก่ไขครับ

--- lab1minx.c
+++ lab1minx.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-int main()
-{
+int main(){
     int n1, n2; 
     printf("Please enter n1 and n2 : ");
     scanf("%d %d", &n1, &n2);
-    printf("You entered: n1 = %d, n2 = %d\n", n1, n2);
-    return 0;
+	if (n1 > n2) {
+    printf("number1 is greater than number2 = %d\n", n1 - n2);
+   }else{
+    printf("number2 is greater than number1 = %d\n", n2 - n1);
+   }
 }
